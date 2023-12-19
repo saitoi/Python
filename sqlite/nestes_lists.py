5e2795f26fcfc01d6aaf4c62bb29b86353f95e44
saitoi <pedrohhs@dcc.ufrj.br>
2023-12-18 23:40:25 -0300

    Some new files, training

---
+++ sqlite/nestes_lists.py
@@ -0,0 +1,18 @@
+menu = [
+    ["egg", "bacon"],
+    ["egg", "sausage", "bacon"],
+    ["egg", "spam"],
+    ["egg", "bacon", "spam"],
+    ["egg", "bacon", "sausage", "spam"],
+    ["spam", "bacon", "sausage", "spam"],
+    ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
+    ["spam", "egg", "spam", "spam", "bacon", "spam"],
+]
+
+for meal in menu:
+    size = len(meal)-1
+    for index, food in enumerate(reversed(meal)):
+        if food == "spam":
+            del meal[size - index]
+        else:
+            print(food)
