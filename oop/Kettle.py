bb0934f4c431217c9c5ac51be16ba2fbc724d45e
saitoi <pedrohhs@dcc.ufrj.br>
2023-12-19 18:02:05 -0300

    Changing directory

--- oop/Kettle.py
+++ oop/Kettle.py
@@ -1,10 +1,21 @@
 class Kettle(object):
 
+    power_source = "electricity"
+
     def __init__(self, make, price):
         self.make = make
         self.price = price
         self.on = False
 
+    def switch(instance):
+        instance.on = not instance.on
+
+
 kenwood = Kettle("Kenwood", 8.99)
+kettle1 = Kettle("whatever_type", 9)
 print(kenwood.make)
 print(kenwood.price)
+print(kenwood.on)
+kenwood.power_source = "gas"
+print(Kettle.power_source)
+print(kenwood.power_source)
--- sqlite/jukebox.py
+++ sqlite/jukebox.py
@@ -6,7 +6,7 @@ SONG_TITLE_INDEX = 1
 def valid_input(lists):
     choice = input()
     if choice.isdigit() and 1 <= int(choice) <= len(lists):
-        return int(choice)
+        return int(choice) - 1  # Adjust the index to start from 0
     else:
         print("Exiting jukebox..")
         exit(1)
@@ -15,10 +15,13 @@ while True:
     print("Please choose your album (invalid choice exits):")
     for index, (title, artist, year, songs) in enumerate(albums):
         print(f"{index + 1}: {title}")
-    choice = valid_input(albums)
+    album_choice = valid_input(albums)
+
     print("Please choose your song:")
-    for index, song in albums[choice][SONG_LIST_INDEX]:
-        print(f"{index + 1}: {song}")
-    choice =  valid_input(albums[choice][SONG_LIST_INDEX])
-    print(f"Playing {albums[choice][SONG_LIST_INDEX][choice - 1][SONG_TITLE_INDEX]}")
+    for index, song in enumerate(albums[album_choice][SONG_LIST_INDEX]):
+        print(f"{index + 1}: {song[SONG_TITLE_INDEX]}")  # Access the song title correctly
+    song_choice = valid_input(albums[album_choice][SONG_LIST_INDEX])
+
+    chosen_song = albums[album_choice][SONG_LIST_INDEX][song_choice]
+    print(f"Playing {chosen_song[SONG_TITLE_INDEX]}")
     print("=" * 40)
