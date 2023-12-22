fb5a01745faa86411c7a423c38f47d5e04145019
saitoi <pedrohhs@dcc.ufrj.br>
2023-12-22 18:32:57 -0300

    Criando um browser simples para um banco de dados

--- tkinter/calculator_correct.py
+++ tkinter/calculator_correct.py
@@ -46,13 +46,13 @@ row = 0
 for keyRow in keys:
     col = 0
     for key in keyRow:
-        tkinter.Button(
+        button_tmp = tkinter.Button(
             keyPad, text=key[0],
-            command=lambda k=key[0]: update_display(k) if k != '=' else calculate()
-        ).grid(
+            command=lambda k=key[0]: update_display(k) if k != '=' else calculate())
+        button_tmp.grid(
             row=row, column=col, columnspan=key[1],
-            sticky=tkinter.E + tkinter.W
-        )
+            sticky=tkinter.E + tkinter.W)
+        button_tmp.config(border=1, relief='raised')
         col += key[1]
     row += 1
 
